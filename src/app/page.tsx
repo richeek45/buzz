0bd80749fc9a793b108849c736e50949931344fd
richeek45 <richeek45@gmail.com>
2024-04-08 18:43:42 +0530

    uncommented session related code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ import { api } from "~/trpc/server";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
-    // const allPosts = await api.post.getAllPosts();
-  // const session = await getServerAuthSession();
+    const allPosts = await api.post.getAllPosts();
+  const session = await getServerAuthSession();
 
-    // console.log(session, allPosts, "session") 
+    console.log(session, allPosts, "session") 
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
@@ -25,19 +25,19 @@ export default async function Home() {
           </p>
 
           <div className="flex flex-col items-center justify-center gap-4">
-            {/* <p className="text-center text-2xl text-white">
+            <p className="text-center text-2xl text-white">
               {session && <span>Logged in as {session.user?.name}</span>}
-            </p> */}
-            {/* <Link
+            </p>
+            <Link
               href={session ? "/api/auth/signout" : "/api/auth/signin"}
               className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
             >
               {session ? "Sign out" : "Sign in"}
-            </Link> */}
+            </Link>
           </div>
         </div>
 
-        {/* <CrudShowcase /> */}
+        <CrudShowcase />
       </div>
     </main>
   );
